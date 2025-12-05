0ab03d4362c21eaddd833c97ebac4dbb7ef3fb4b
Mikasa <micosa114@gmail.com>
2025-12-05 20:01:46 +0700

    devc

--- resume_thread.c
+++ resume_thread.c
@@ -5,28 +5,31 @@ int main(){
     STARTUPINFO si = {sizeof(si)};
     PROCESS_INFORMATION pi = {0};
 
-    si.dwFlags = STARTF_USESHOWWINDOW;
-    si.wShowWindow = SW_SHOWMINIMIZED;
-
     if(CreateProcess("C:\\Windows\\System32\\notepad.exe", 
         NULL, 
         NULL, 
         NULL, 
-        FALSE,
-        0, 
+        FALSE, 
+        CREATE_SUSPENDED, 
         NULL, 
         NULL, 
-        &si, &pi
-    )) {
+        &si, &pi)){
+        
         printf("%lu\n", pi.dwProcessId);
         printf("%lu\n", pi.dwThreadId);
+        printf("%lu\n", pi.hProcess);
+        printf("%lu\n", pi.hThread);
+
+        getchar();
+
+        ResumeThread(pi.hThread);
 
         CloseHandle(pi.hProcess);
         CloseHandle(pi.hThread);
-    }
-    else {
+    } else {
         printf("Error: %lu\n", GetLastError());
+        return 1;
     }
 
-return 0;
+    return 0;
 }
